b46d5952dad1b36255eeb79855f654259798c452
GiovanniJairSanchezChimal <ghostgs.117@gmail.com>
2025-03-04 00:42:35 -0600

    Creacion de actializar y eliminar post

--- app.py
+++ app.py
@@ -57,3 +57,17 @@ def add_post():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+#Actualizar post
+@app.route('/post/update/<int:id>', methods=['GET','POST'])
+def update_post(id):
+    post = Post.query.get(id)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.category_id = request.form['category_id']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect(url_for('index'))
+    
+    categories = Category.query.all()
+    return render_template('update_post.html', post=post, categories=categories)
